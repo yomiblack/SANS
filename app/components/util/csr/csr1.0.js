36848473a139d3970bf76f864982c5df9b4521e4
Yomi Black <Yomiblacck@gmail.com>
2025-12-08 06:38:30 -0800

    add reuseable gallery template and gallery cards code refactor

---
+++ app/components/util/csr/csr1.0.js
@@ -0,0 +1,10 @@
+"use server";
+export default async function csr1() {
+    const csr1 = [
+        {
+            name: "Back to School Project",
+            videoURL: "https://www.youtube.com/watch?v=s-6KuHXE-Wg",
+        },
+    ];
+    return csr1;
+}
